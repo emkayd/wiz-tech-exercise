c76cf2a74dd6391ec6b754203b036b4b768fc787
emkayd <minookarthik@gmail.com>
2025-11-20 10:57:31 -0600

    Add S3 backend for TF state

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "wiz-cloudlabs-tfstate-1234"  # <-- your actual bucket name
+    bucket = "wiz-cloudlabs-tfstate-92837" # <-- your bucket name
     key    = "infra/terraform.tfstate"
     region = "us-east-1"
   }
