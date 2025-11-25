6f852c24f98c9be083de79a28cb7dc0c34f43178
emkayd <minookarthik@gmail.com>
2025-11-25 13:57:49 -0600

    Quick Demo Update

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,6 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # We fixed this to allow 6.x, not force 5.x
       version = ">= 6.0, < 7.0"
     }
     kubernetes = {
